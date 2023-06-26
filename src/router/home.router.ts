90ebb7fec88b0e432627bbc6d9ee7cc5592f20f8
Lã Ngọc Hải <langochai11121998@gmail.com>
2023-06-26 15:19:36 +0700

    Revert "Search"

--- src/router/home.router.ts
+++ src/router/home.router.ts
@@ -1,5 +1,4 @@
 import { HomeController } from '../controller/home.Controller';
 import express from 'express';
 export const router = express.Router();
-
-router.get("/", HomeController.showHome);
+router.get("/home", HomeController.showHome);
--- src/router/search.router.ts
+++
@@ -1,4 +0,0 @@
-import { SearchController } from '../controller/search.Controller';
-import express from 'express';
-export const router = express.Router();
-router.post("/", SearchController.productsSearch);

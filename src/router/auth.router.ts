2300e581bf9ad17e3ac1739a0ac621e17274badd
NDKhanh1996 <ndkhanh1996@gmail.com>
2023-06-26 09:30:44 +0700

    fix authrouter: combine alertcontrol and showFormSignin to one line

--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -5,6 +5,7 @@ export const router = express.Router();
 router.get("/signup", AuthController.showSignupPage);
 router.post("/signup", AuthController.createAccount);
 
+router.get("/signin", AuthController.alertControl, AuthController.showFormSignin);
 router.post('/signin', (req, res, next) => {
     passport.authenticate('local', (err: any, user: any, info: any) => {
         if (err) return next(err);
@@ -15,11 +16,8 @@ router.post('/signin', (req, res, next) => {
         res.locals.user = req.user;
     })(req, res, next);
 });
+
 router.get('/logout', (req, res)=>{
     req.logOut;
     res.redirect('/home');
-})
-
-router.use(AuthController.alertControl);
-router.get("/signin", AuthController.showFormSignin);
-
+});

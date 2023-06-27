7d52eaeee70d9b096184c8807b8d372a927fe501
NDKhanh1996 <ndkhanh1996@gmail.com>
2023-06-27 13:55:26 +0700

    clean code

--- src/schemas/data-source.ts
+++ src/schemas/data-source.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export class Database {
     static async connectDB() {
-        const DB_URL = process.env.DB || "mongodb://127.0.0.1:27017/SuperMarket"
+        const DB_URL = process.env.DB || "mongodb+srv://Viet:123456!@cluster0.cznxnof.mongodb.net/SuperMarket";
         return await mongoose.connect(DB_URL);
     }
 }

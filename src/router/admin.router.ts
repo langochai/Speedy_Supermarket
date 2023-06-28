0eb48fafe2bd582c830a76a5fb3a462df40934f6
langochai <langochai11121998@gmail.com>
2023-06-28 09:30:36 +0700

    delete multer for firebase usage

--- src/router/admin.router.ts
+++ src/router/admin.router.ts
@@ -1,24 +1,14 @@
-import { AdminManagement } from "../controller/admin/adminManagement";
 import express from 'express';
-import multer from 'multer';
+import { AdminManagement } from "../controller/admin/adminManagement";
 import { AuthorizeMiddleware } from "../middlewares/authorize.middleware";
 import { AdminShowUserList } from "../controller/admin/adminShowUserList";
-const storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, 'public/index/uploads/');
-    },
-    filename: function (req, file, callback) {
-        callback(null, req.body.productName + req.body.price + file.originalname);
-    }
-})
-const upload = multer({ storage: storage });
 export const router = express.Router();
 router.use(AuthorizeMiddleware.authorizedAdmin)
 router.get("/", AdminManagement.showAdminHomePage);
 router.get("/product/add/", AdminManagement.getAdminAddProduct)
-router.post("/product/add/", upload.single('image'), AdminManagement.postAdminAddProduct)
+router.post("/product/add/", AdminManagement.postAdminAddProduct)
 router.get("/product/update/:id", AdminManagement.getAdminUpdateProduct)
-router.post("/product/update/:id", upload.single('image'), AdminManagement.postAdminUpdateProduct)
+router.post("/product/update/:id", AdminManagement.postAdminUpdateProduct)
 router.get("/product/delete/:id", AdminManagement.getAdminDeleteProduct)
 router.post("/product/delete/:id", AdminManagement.postAdminDeleteProduct)
 router.post("/product/search/:name", AdminManagement.adminSearchProduct)

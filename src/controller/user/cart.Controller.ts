4b94f97a68d9cca17073440ecc80feda4e6bee04
unknown <leong.dev.35@gmail.com>
2023-06-29 09:21:27 +0700

    add, delete, show cart

--- src/controller/user/cart.Controller.ts
+++ src/controller/user/cart.Controller.ts
@@ -2,209 +2,167 @@ import { Cart } from "../../schemas/user.schemas/cart.model";
 import { User } from "../../schemas/user.schemas/user.model";
 import { Product } from "../../schemas/product.schemas/product.model";
 
-
-async function getCart(user_id, purchased: boolean) {
-    const userCart = await User.findById(user_id).populate({ path: 'cart', model: Cart });
-    //@ts-ignore
-    let userCartFilter = userCart.cart.filter(item => item.purchased === purchased)
-    return userCartFilter; //! trả về một mảng các cart có trạng thái 
-
+async function getCartByUserAndPurchaseStatus(user_id, purchased: boolean) {
+    try {
+        const userCart = await User.findById(user_id).populate({ path: 'cart', model: Cart });
+        //@ts-ignore
+        let userCartFilter = userCart.cart.filter(item => item.purchased === purchased)
+        return userCartFilter;
+    } catch (error) {
+        console.log(error);
+    }
 }
-
-async function getProductList(user_id) {
-    // const userId = user_id; // Replace USER_ID with the actual ID of the user
-    // const userCart = await User.findById(userId).populate({path: 'cart', model: Cart}); //! cần check xem cart đã thanh toán chưa. Lấy ra 
-
-    //! lấy danh sách cart chưa thanh toán
-    const userCart = await getCart(user_id, false);
+async function getProductListInCart(user_id) {
+    const userCartUnpaid = await getCartByUserAndPurchaseStatus(user_id, false);
     //@ts-ignore
-    const detail = userCart[0].detail
+    const detail = userCartUnpaid[0].detail
     let arrProduct = detail.map(item => item.product);
-    console.log(arrProduct);
-    let productList = await Product.find({ _id: { $in: arrProduct } })
-    console.log(productList); //! lấy ra được 1 mảng các product trong cart để hiển thị lên view
+    let productList;
+    //@ts-ignore
+    await Product.find({ _id: { $in: arrProduct } })
+        .lean()
+        .then(function (results) {
+            // Thực hiện các thay đổi trên kết quả
+            results.forEach(function (result) {
+                detail.forEach((element) => {
+                    if (element.product.toString() == result._id.toString()) {
+                        //@ts-ignore
+                        result.productQuantity = element.productQuantity;
+                    }
+                });
+            });
+            productList = results;
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
     return productList;
 }
 
-async function productForCart(user_id, request) {
-    let product_id = "64991567ed2bb8ab40e44616"; //! id của sản phẩm trong cart, id sẽ lấy từ param
-    // let product_id = request.param; 
-    let userCartUnPaid = await getCart(user_id, false); //! lấy cart chưa thanh toán, có duy nhất 1 cart
-
-    //! check request
-    switch (request.method) {
-        case "GET":
-            //! test tạm ở phương thức get
+async function productForCart(user_id, productId, request) {
+    let product_id = productId; 
+    let userCartUnPaid = await getCartByUserAndPurchaseStatus(user_id, false);
+    //@ts-ignore
+    const detail = userCartUnPaid[0].detail
+    const index = detail.findIndex(obj => obj.product == product_id);
+    if ('id' in request.params) {
+        if (index !== -1) {
+            console.log(`ID  đã được tìm thấy trong mảng`);
             //@ts-ignore
-            const detail = userCartUnPaid[0].detail
-            const index = detail.findIndex(obj => obj.product == product_id);
-            //! chức năng cho PUT
-            //    if (index !== -1) {
-            //     console.log(`ID  đã được tìm thấy trong mảng`);
-            //     //@ts-ignore
-            // //! lấy ra số lượng của product
-            //    await Cart.findOneAndUpdate({_id: userCartUnPaid[0]._id},
-            //     { $inc: { [`detail.${index}.productQuantity`]: 1 }}, //! tăng sản phẩm hiện tại lên 1 nếu tìm thấy trong giỏ hảng
-            //     { new: true } 
-            //      ).then((updatedCart) => {
-            //         console.log(updatedCart,123);
-            //       })
-            //       .catch((error) => {
-            //         console.log(error);
-            //       });
-
-            //   } else {
-            //     console.log(`ID  không tìm thấy trong mảng`);
-            //     let newProduct = {
-            //         product : product_id,
-            //         productQuantity : 1,
-            //     }
-            //     //@ts-ignore
-            //     await Cart.findById({_id: userCartUnPaid[0]._id})
-            //         .then((cart) => {
-            //             cart.detail.push(newProduct); // Sử dụng push() để thêm đối tượng vào mảng
-
-            //             cart.save(); //! Lưu sản phẩm mới thành công
-            //         })
-            //   };
-            //! chức năng cho DELETE
-            if (index !== -1) {
-                console.log(`ID  đã được tìm thấy trong mảng`); //! đối tượng product có tồn tại
-                //@ts-ignore
-                //! lấy ra số lượng của product
-
-                await Cart.findById({ _id: userCartUnPaid[0]._id })
-                    .then((cart) => {
-                        console.log(cart);
-                        //@ts-ignore
-
-                        const child = cart.detail.find(child => child.product == product_id);
-                        console.log(child);
-
-                        //@ts-ignore
-                        cart.detail.pull(child); // Sử dụng push() để thêm đối tượng vào mảng
-                        console.log("đã xóa thành công");
-
-                        cart.save(); //! Lưu sản phẩm mới thành công
-                    })
-
-            } else {
-                console.log(`ID  không tìm thấy trong mảng`);
-
-            };
-
-
-
-
-            break;
-        case "PUT": //! cập nhật lại Detail trong Cart (thêm sản phẩm mới, tăng số lượng or giảm số lượng sản phẩm đã tồn tại)
-            //! thêm sản phẩm mới: check xem đã có trong cart chưa. Nếu có tăng só lượng đang có lên 1. Nếu không thêm mới với số lượng là 1
-
-            break;
-        case "DELETE":
-
-            break;
-        default:
-            break;
+            await Cart.findOneAndUpdate({ _id: userCartUnPaid[0]._id },
+                { $inc: { [`detail.${index}.productQuantity`]: 1 } }, 
+                { new: true }
+            ).then((updatedCart) => {
+            })
+                .catch((error) => {
+                    console.log(error);
+                });
+        } else {
+            console.log(`ID  không tìm thấy trong mảng`);
+            let newProduct = {
+                product: product_id,
+                productQuantity: 1,
+            }
+            //@ts-ignore
+            await Cart.findById({ _id: userCartUnPaid[0]._id })
+                .then((cart) => {
+                    cart.detail.push(newProduct);
+                    cart.save();
+                })
+        };
+    } else if ('idDelete' in request.params) {
+        if (index !== -1) {
+            console.log(`ID  đã được tìm thấy trong mảng`); 
+            //@ts-ignore
+             await Cart.findById({ _id: userCartUnPaid[0]._id })
+                .then((cart) => {
+                    //@ts-ignore
+                    const child = cart.detail.find(child => child.product == product_id);
+                    //@ts-ignore
+                    cart.detail.pull(child); // Sử dụng push() để thêm đối tượng vào mảng
+                    console.log("đã xóa thành công");
+                    cart.save(); 
+                })
+        } else {
+            console.log(`ID  không tìm thấy trong mảng`);
+        };
     }
 }
-
-async function changeProductQuantity(user_id, request, inOrDe) { //! inOrDe là tăng or giảm
-    let product_id = "64991567ed2bb8ab40e44615"; //! id của sản phẩm trong cart, id sẽ lấy từ param
-    // let product_id = request.param; 
-    let userCartUnPaid = await getCart(user_id, false); //! lấy cart chưa thanh toán, có duy nhất 1 cart
-    console.log(typeof inOrDe);
+async function changeProductQuantity(user_id, request, inOrDe) {
+    let product_id = "64991567ed2bb8ab40e44615"; 
+    let userCartUnPaid = await getCartByUserAndPurchaseStatus(user_id, false);
     let test;
-    if(inOrDe == "true") {
-        test = 1; //! giả sử là tăng
+    if (inOrDe == "true") {
+        test = 1; 
     } else {
         test = -1; //giảm
     }
-    console.log(test);
-    
     //@ts-ignore
     const detail = userCartUnPaid[0].detail
     const index = detail.findIndex(obj => obj.product == product_id);
     if (index !== -1) {
         console.log(`ID  đã được tìm thấy trong mảng`);
         //@ts-ignore
-        //! lấy ra số lượng của product
         await Cart.findOneAndUpdate({ _id: userCartUnPaid[0]._id },
-            { $inc: { [`detail.${index}.productQuantity`]: test } }, //! tăng sản phẩm hiện tại lên 1 nếu tìm thấy trong giỏ hảng
+            { $inc: { [`detail.${index}.productQuantity`]: test } }, 
             { new: true }
         ).then((updatedCart) => {
-            console.log(updatedCart, 123);
         })
             .catch((error) => {
                 console.log(error);
             });
-
     } else {
         console.log(`ID  không tìm thấy trong mảng`);
 
     };
-
-
 }
-
 async function purchase(cart_id) {
-    // await Cart.findById({ _id: cart_id })
-    //                 .then((cart) => {
-    //                     console.log("đã tìm thây cart");
-                        
-    //                     console.log(cart,123);
-    //                     // console.log(cart);
-                        
-    //                     //@ts-ignore
-    //                     const newPurchased = true;
-    //                     cart.purchased
-    //                     // const child = cart.detail.find(child => child.product == product_id);
-    //                     // console.log(child);
-
-    //                     // //@ts-ignore
-    //                     // cart.detail.pull(child); // Sử dụng push() để thêm đối tượng vào mảng
-    //                     // console.log("đã xóa thành công");
-
-    //                     // cart.save(); //! Lưu sản phẩm mới thành công
-    //                 })
-    //                 .catch(err => console.log("khong tim thay cart"))
-    console.log(typeof cart_id);
-    console.log(cart_id);
-    
-    
     try {
         const cart = await Cart.findOne({ _id: cart_id });
-    if (cart) {
-      console.log('Cart found');
-      await Cart.updateOne(
-        { _id: cart_id },
-        { $set: { purchased: true } },
-        { upsert: false }
-     )
-    } else {
-      console.log('Cart not found');
-    }
-      } catch (error) {
+        if (cart) {
+            console.log('Cart found');
+            await Cart.updateOne(
+                { _id: cart_id },
+                { $set: { purchased: true } },
+                { upsert: false }
+            )
+        } else {
+            console.log('Cart not found');
+        }
+    } catch (error) {
         console.error('Error occurred:', error);
         console.log("Id cart không đúng định dạng");
-        
-      }
-    
+    }
+
 }
-
-
 export class CartController {
     static async showCart(req, res) {
-        getProductList('649900d7bb759754879c9d38') //! user_id
+        try {
+            let productList = await getProductListInCart(req.user._id)
+            res.render('cart/cart', { productList });
+        } catch (error) {
+            console.log(error);
+            res.redirect('/home')
+        }
     }
     static async addProductToCart(req, res) {
-        productForCart('649900d7bb759754879c9d38', req)
+        try {
+            if (req.params.id) {
+                await productForCart(req.user._id, req.params.id, req)
+                res.redirect('/home');
+            } else if (req.params.idDelete) {
+                await productForCart(req.user._id, req.params.idDelete, req)
+                res.redirect('/cart');
+            }
+        } catch (error) {
+            console.log(error);
+            res.redirect('/home')
+        }
 
     }
     static async changeProductQuantity(req, res) {
-        changeProductQuantity('649900d7bb759754879c9d38', req, req.params.check)
+        changeProductQuantity(req.user._id, req, req.params.check)
     }
-
     static async purchase(req, res) {
         purchase(req.params.cartID)
     }
--- src/router/cart.router.ts
+++ src/router/cart.router.ts
@@ -2,7 +2,8 @@ import { CartController } from '../controller/user/cart.Controller';
 import express from 'express';
 export const router = express.Router();
 router.get("/", CartController.showCart);
-router.get("/123", CartController.addProductToCart);
-router.get("/1234-:check", CartController.changeProductQuantity);
+router.get("/add/:id", CartController.addProductToCart);
+router.get("/delete/:idDelete", CartController.addProductToCart);
+router.get("/:id-:check", CartController.changeProductQuantity);
 router.get("/:cartID", CartController.purchase);
 
